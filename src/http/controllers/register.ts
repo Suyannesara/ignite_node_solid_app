1950fb1f7a0073ca68229d0b2ce2d6b3f596eeae
suyanne <suyannesara17@gmail.com>
2024-01-08 11:31:41 -0300

    ♻️ isolate prisma things

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import z from 'zod'
 import { RegisterService } from '@/services/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
 export default async (request: FastifyRequest, reply: FastifyReply) => {
   const registerBodySchema = z.object({
