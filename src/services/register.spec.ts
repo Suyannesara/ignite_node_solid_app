6bf0117d83d549653a646cccb44227ed1e6c2c9c
suyanne <suyannesara17@gmail.com>
2024-01-09 09:58:24 -0300

    🧪 first unit test

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,37 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterService } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Service', () => {
+  it('should hash user password upon registration', async () => {
+    const registerService = new RegisterService({
+      async findByEmail() {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-id',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const password = '123456'
+
+    const { user } = await registerService.execute({
+      name: 'test',
+      email: 'test@test3.com',
+      password,
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      password,
+      user?.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
